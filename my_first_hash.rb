a96bb4cbf5947299c21f96903cbddaccbce79828
Monica Marziano <mmarziano1@gmail.com>
2018-04-09 09:15:27 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 my_hash = {"name" => "Monica", "age" => "don't ask", "occupation" => "badass"}
+
+return my_hash
   
 end
 
